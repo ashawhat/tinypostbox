e326aeadb4a2557c0900f18b1667a98c043d3cfc
ashawhat <ashlcycle@gmail.com>
2014-05-14 17:02:25 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FriendCraft::Application.config.secret_key_base = '461d513db678dd68f17f74b999f75aa30b7dbf4516c18e8640b3144c3a64ea8931220c2ea7fd8fa1a4553edf0f3198c1fdc5cfff30b1a17e28ac758e15df6e2c'
